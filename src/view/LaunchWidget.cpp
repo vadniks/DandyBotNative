a58388131982608df2567f5d7d9ed73df3d2dc64
vadniks <whatever@fake.some>
2023-02-07 18:03:04 +0300

    Added copyrights

--- src/view/LaunchWidget.cpp
+++ src/view/LaunchWidget.cpp
@@ -1,3 +1,12 @@
+/*
+ * Inspired by the work of Peter Sovietov's DandyBot
+ * (https://github.com/true-grue/DandyBot) which was written in 2021-2022.
+ *
+ * Ported to the QT framework by Vad Nik
+ * (https://github.com/vadniks/DandyBotNative) in 2023 for educational purpose.
+ *
+ * TODO: add licence
+ */
 
 #include "LaunchWidget.hpp"
 #include "../consts.hpp"
